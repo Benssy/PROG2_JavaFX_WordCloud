30199505f9b521c3b137a7d8f8c18fefa468e8a3
kolatben <kolatben@students.zhaw.ch>
2022-03-27 16:41:49 +0200

    added fxml files

---
+++ app/src/main/java/ch/zhaw/prog2/application/MainWindowController.java
@@ -0,0 +1,27 @@
+package ch.zhaw.prog2.application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class MainWindowController {
+    @FXML
+    TextField textEingabe;
+    @FXML
+    Label lableTitel;
+    @FXML
+    TextArea textHistory;
+    @FXML
+    Button add;
+    @FXML
+    Button delete;
+
+    public void hinzufuegenText(ActionEvent actionEvent) {
+    }
+
+    public void leerenTextEingabe(ActionEvent actionEvent) {
+    }
+}
